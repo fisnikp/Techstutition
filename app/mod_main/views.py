167cdd9e5b3056c81a00f7534c1995fe03cfc5f7
fisnikp <fisnikk.p@gmail.com>
2017-02-23 16:47:19 +0100

    lidhja me json hapi 3 u kry, vazhdim e siper hapi 4..

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -31,104 +31,80 @@ def form():
           },
 		   "linjat_me_qera":{ #hapi-2
 		      "ofruara":{
-		         "kombtare":{
-		            "njesia":form_data['njesia_kombetare'],
-		            "sasia":form_data['sasia_kombetare'],
-					"komente":form_data['komente_kombetare']
-		         }
-
+			    "kombtare/nderkombtare" : form_data['Nr_total_linja_qira_ofruara_rrjete_tjera_1'],
+	            "njesiaofruara":form_data['njesia_ofruara'],
+	            "sasiaofruara":form_data['sasia_ofruara'],
+				"komenteofruara":form_data['komente_ofruara']
 		      },
 		      "te_marra":{
-		         "kombtare":{
-		            "njesia":form_data['temarra_njesia_kombetare'],
-		            "sasia":form_data['temarra_sasia_kombetare'],
-					"komente":form_data['temarra_komente_kombetare']
-		         }
+			    "kombtare/nderkombtare2" : form_data['Nr_total_linja_qira_ofruara_rrjete_tjera_2'],
+	            "temarranjesia":form_data['temarra_njesia'],
+	            "temarrasasia":form_data['temarra_sasia'],
+				"temarrakomente":form_data['temarra_komente']
 			  }
 	      },
-	       "Kapaciteti_total":{ #hapi3
-	            "deri_ne_dhe_perfshire_64Kb/s":{
-	               "segmente_terminuese":form_data['segmente_terminuese'],
-	               "trunk_segmente":form_data['trunk_segmente'],
-	               "segment_nderkombetare":form_data['segment_nderkombetare'],
-	               "te_tjera":form_data['te_tjera'],
-	               "perkufizime_te_tjera":form_data['perkufizime_te_tjera'],
-	               "te_tjera_2":form_data['te_tjera_2'],
-	               "Perkufizime_2_te_tjera":form_data['Perkufizime_2_te_tjera'],
-	               "komente":form_data['komentet']
-	            },
-			    "deri_dhe_perfshire_2Mb/s":{
-	               "segmente_terminuese":form_data['segmente_terminuese'],
-	               "trunk_segmente":form_data['trunk_segmente'],
-	               "segment_nderkombetare":form_data['segment_nderkombetare'],
-	               "te_tjera":form_data['te_tjera'],
-	               "perkufizime_te_tjera":form_data['perkufizime_te_tjera'],
-	               "te_tjera_2":form_data['te_tjera_2'],
-	               "Perkufizime_2_te_tjera":form_data['Perkufizime_2_te_tjera'],
-	               "komente":form_data['komentet']
-	            },
-	            "deri_ne_dhe_perfshire_34Mb/s":{
-	               "segmente_terminuese":form_data['segmente_terminuese'],
-	               "trunk_segmente":form_data['trunk_segmente'],
-	               "segment_nderkombetare":form_data['segment_nderkombetare'],
-	               "te_tjera":form_data['te_tjera'],
-	               "perkufizime_te_tjera":form_data['perkufizime_te_tjera'],
-	               "te_tjera_2":form_data['te_tjera_2'],
-	               "Perkufizime_2_te_tjera":form_data['Perkufizime_2_te_tjera'],
-	               "komente":form_data['komentet']
-	            },
-	            "140_Mbps":{
-	               "segmente_terminuese":form_data['segmente_terminuese'],
-	               "trunk_segmente":form_data['trunk_segmente'],
-	               "segment_nderkombetare":form_data['segment_nderkombetare'],
-	               "te_tjera":form_data['te_tjera'],
-	               "perkufizime_te_tjera":form_data['perkufizime_te_tjera'],
-	               "te_tjera_2":form_data['te_tjera_2'],
-	               "Perkufizime_2_te_tjera":form_data['Perkufizime_2_te_tjera'],
-	               "komente":form_data['komentet']
-	            },
-	            "STM_1":{
-	               "segmente_terminuese":form_data['segmente_terminuese'],
-	               "trunk_segmente":form_data['trunk_segmente'],
-	               "segment_nderkombetare":form_data['segment_nderkombetare'],
-	               "te_tjera":form_data['te_tjera'],
-	               "perkufizime_te_tjera":form_data['perkufizime_te_tjera'],
-	               "te_tjera_2":form_data['te_tjera_2'],
-	               "Perkufizime_2_te_tjera":form_data['Perkufizime_2_te_tjera'],
-	               "komente":form_data['komentet']
-	            },
-	            "STM_16":{
-	               "segmente_terminuese":form_data['segmente_terminuese'],
-	               "trunk_segmente":form_data['trunk_segmente'],
-	               "segment_nderkombetare":form_data['segment_nderkombetare'],
-	               "te_tjera":form_data['te_tjera'],
-	               "perkufizime_te_tjera":form_data['perkufizime_te_tjera'],
-	               "te_tjera_2":form_data['te_tjera_2'],
-	               "Perkufizime_2_te_tjera":form_data['Perkufizime_2_te_tjera'],
-	               "komente":form_data['komentet']
-	            },
-	            "STM_64":{
-	               "segmente_terminuese":form_data['segmente_terminuese'],
-	               "trunk_segmente":form_data['trunk_segmente'],
-	               "segment_nderkombetare":form_data['segment_nderkombetare'],
-	               "te_tjera":form_data['te_tjera'],
-	               "perkufizime_te_tjera":form_data['perkufizime_te_tjera'],
-	               "te_tjera_2":form_data['te_tjera_2'],
-	               "Perkufizime_2_te_tjera":form_data['Perkufizime_2_te_tjera'],
-	               "komente":form_data['komentet']
-	            },
-	            "STM_tjera":{
-	               "segmente_terminuese":form_data['segmente_terminuese'],
-	               "trunk_segmente":form_data['trunk_segmente'],
-	               "segment_nderkombetare":form_data['segment_nderkombetare'],
-	               "te_tjera":form_data['te_tjera'],
-	               "perkufizime_te_tjera":form_data['perkufizime_te_tjera'],
-	               "te_tjera_2":form_data['te_tjera_2'],
-	               "Perkufizime_2_te_tjera":form_data['Perkufizime_2_te_tjera'],
-	               "komente":form_data['komentet']
-	            }
-	         }
+		  "Kapaciteti_total":{ #hapi3
+	               "llojet_kapacitetev_linjav_meqera_nrrjet":form_data['Kapaciteti_total_furnizimit_linjave_me_qira_ne_rrjet'],
+	               "segmente_terminuese_":form_data['segmente_terminuese'],
+	               "trunk_segmente_":form_data['trunk_segmente'],
+	               "segment_nderkombetare_":form_data['segment_nderkombetare'],
+	               "te_tjera_":form_data['te_tjera'],
+	               "perkufizime_te_tjera_":form_data['perkufizime_te_tjera'],
+	               "te_tjera_2_":form_data['te_tjera_2'],
+	               "Perkufizime_2_te_tjera_":form_data['Perkufizime_2_te_tjera'],
+	               "komente_":form_data['komentet']
+	     },
+		 "Llojet_e_Linjave_me_qira_shperndara_perdoruesit_pakice":{ #************hapi 4
+			       "Linja_Analoge_me_qira":{
+			            "Njesia": form_data['njesia_analoge'],
+			            "10KM":form_data['10km_analoge'],
+			            "10KM_deri_50km":form_data['10KM_deri_50km_analoge'],
+			            "51_deri_100km":form_data['51KM_deri_100km_analoge'],
+			            "100km":form_data['100km_analoge'],
+			            "komente":form_data['komente_analoge']
+			      },
+			       "Linja_digjitale":{
+					    "Njesia": form_data['njesia_digjitale'],
+			            "10KM":form_data['10km_digjitale'],
+			            "10KM_deri_50km":form_data['10KM_deri_50km_digjitale'],
+			            "51_deri_100km":form_data['51KM_deri_100km_digjitale'],
+			            "100km":form_data['100km_digjitale'],
+			            "komente":form_data['komente_digjitale']
+			      },
+			      "Deri_ne_64_kbps":{
+				        "Njesia": form_data['njesia_64kbps'],
+			            "10KM":form_data['10km_64kbps'],
+			            "10KM_deri_50km":form_data['10KM_deri_50km_64kbps'],
+			            "51_deri_100km":form_data['51KM_deri_100km_64kbps'],
+			            "100km":form_data['100km_64kbps'],
+			            "komente":form_data['komente_64kbps']
+			     },
+			     "64_kbps_deri_ne_2mbps":{
+			            "Njesia": form_data['njesia_2mbps'],
+			            "10KM":form_data['10km_2mbps'],
+			            "10KM_deri_50km":form_data['10KM_deri_50km_2mbps'],
+			            "51_deri_100km":form_data['51KM_deri_100km_2mbps'],
+			            "100km":form_data['100km_2mbps'],
+			            "komente":form_data['komente_2mbps']
+			      },
+			    "2Mbits_deri_ne_34Mbps":{
+			  	        "Njesia": form_data['njesia_34mbps'],
+			            "10KM":form_data['10km_34mbps'],
+			            "10KM_deri_50km":form_data['10KM_deri_50km_34mbps'],
+			            "51_deri_100km":form_data['51KM_deri_100km_34mbps'],
+			            "100km":form_data['100km_34mbps'],
+			            "komente":form_data['komente_34mbps']
+			      },
+			    "34Mbps_deri_140_MBPS": {
+			            "Njesia": form_data['njesia_140mbps'],
+			            "10KM":form_data['10km_140mbps'],
+			            "10KM_deri_50km":form_data['10KM_deri_50km_140mbps'],
+			            "51_deri_100km":form_data['51KM_deri_100km_140mbps'],
+			            "100km":form_data['100km_140mbps'],
+			            "komente":form_data['komente_140mbps']
+			      }
 		 }
+	  }
 		db.insert(data)
 		return render_template("formulari.html", mesazhi="Falemderit, forma u plotesua" )
 
